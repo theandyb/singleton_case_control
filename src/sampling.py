62dc1271b1f627aac93dc6baa82ff31c41543206
Andy Beck <andy@umich.edu>
2019-09-08 15:08:09 -0400

    fix typo

--- src/sampling.py
+++ src/sampling.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description="Sample control distribution.")
     parser.add_argument("-s", "--singleton", help="Location of singleton file", required=True)
     parser.add_argument("-f", "--fasta", help="FASTA file to grab sequence from", required=True)
-    parser.add_argument("-o", "--output", help="Path to output", require=True)
+    parser.add_argument("-o", "--output", help="Path to output", required=True)
     parser.add_argument("chrom", help="Chromosome we are sampling from", required=True)
     args = parser.parse_args()
 
