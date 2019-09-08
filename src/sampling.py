5d275678a44220a090ed8d94dc350e8516048de3
Andy Beck <andy@umich.edu>
2019-09-08 15:03:55 -0400

    Better sampler with parsing of command line args

--- src/sampling.py
+++ src/sampling.py
@@ -2,10 +2,19 @@ from pyfaidx import Fasta
 import re
 import random
 import pandas as pd
+import argparse
 
 def main():
-    ref_file = "reference_data/human_g1k_v37/human_g1k_v37.fasta"
-    singleton_file = "/net/snowwhite/home/beckandy/research/smaug-redux/summaries/singletons.full.summary"
+    parser = argparse.ArgumentParser(description="Sample control distribution.")
+    parser.add_argument("-s", "--singleton", help="Location of singleton file", required=True)
+    parser.add_argument("-f", "--fasta", help="FASTA file to grab sequence from", required=True)
+    parser.add_argument("-o", "--output", help="Path to output", require=True)
+    parser.add_argument("chrom", help="Chromosome we are sampling from", required=True)
+    args = parser.parse_args()
+
+    ref_file = args.fasta #"reference_data/human_g1k_v37/human_g1k_v37.fasta"
+    singleton_file = args.singleton #"/net/snowwhite/home/beckandy/research/smaug-redux/summaries/singletons.full.summary"
+    chrom = args.chrom
     output_list = []
     # Create fasta object
     fasta_obj = Fasta(ref_file, read_ahead=10000, as_raw=True)
@@ -15,30 +24,22 @@ def main():
         line = fp.readline()
         line = fp.readline()
         while line:
-            output_list.extend(process_line(line, fasta_obj))
+            output_list.append(process_line(line, chrom, fasta_obj))
             line = fp.readline()
     final = pd.DataFrame(output_list)
-    final.to_csv("data/sample.csv", index = None, header=True)
+    final.to_csv(args.output, index = None, header=True)
     print("Done!")
 
 
-def process_line(x, fsObj):
-    new_pair = []
+def process_line(x, chrom, fsObj):
     content = x.split("\t")
-    chrom = content[0]
     pos = int(content[1])
     ref = content[2]
-    motif = content[6][:9]
-    new_pair.append({
-        'chrom':chrom,
-        'pos':pos,
-        'motif': motif,
-        'mutation' : 1
-    })
-    new_pair.append(sample_control(chrom, pos, ref, fsObj))
-    return new_pair
+    motif = content[3][:9]
+    cat = content[4]
+    return sample_control(chrom, pos, ref, cat, fsObj)
 
-def sample_control(chrom, pos, ref, fsObj, window=300, bp=4):
+def sample_control(chrom, pos, ref, cat, fsObj, window=300, bp=4):
     seq = fsObj['{}'.format(chrom)][(pos-1-window):(pos-1+window)]
     sites = [m.start() for m in re.finditer(ref, seq)]
     ix = random.choice(sites) + pos - 1 - window
@@ -52,6 +53,7 @@ def sample_control(chrom, pos, ref, fsObj, window=300, bp=4):
         'chrom' : chrom,
         'pos' : ix,
         'motif' : newSeq,
+        'cat': cat,
         'mutation' : 0
     }
     return entry
