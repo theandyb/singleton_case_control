14bde61d39ec3e9b8e246ab741fa7e5bb563e080
Andy Beck <beckandy@umich.edu>
2019-09-11 13:46:03 -0400

    Ignore slurm output

---
+++ src/batch_sample.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=beckandy@umich.edu
+#SBATCH --ntasks=1
+#SBATCH --mem=800
+#SBATCH --time=36:00:00
+#SBATCH --job-name=sampleSingletons
+#SBATCH --partition=nomosix
+#SBATCH --requeue
+
+srun python /net/snowwhite/home/beckandy/research/singleton_case_control/src/sampling.py -s /net/snowwhite/home/beckandy/research/smaug-redux/summaries/chr16_full.singletons -f /net/snowwhite/home/beckandy/research/smaug-redux/reference_data/human_g1k_v37/chr16.fasta.gz -o /net/snowwhite/home/beckandy/research/singleton_case_control/data/chr16.csv -x 1160999 16
